68459c69ed85e08d1ac84404749856e8c61aa0c7
chris <christopher.t.bate1@gmail.com>
2019-11-14 18:30:26 -0700

    Add positive anchor conf tracking to model forward

--- models/detection_model.py
+++ models/detection_model.py
@@ -9,14 +9,14 @@ from models.anchor_generator import AnchorGenerator
 
 class ObjectDetection(torch.nn.Module):
     '''
-        input_image_shape : The size of the input image. 
-                            In our system, this is fixed. For non-fixed systems, it should be 
+        input_image_shape : The size of the input image.
+                            In our system, this is fixed. For non-fixed systems, it should be
                             the average or median size.
 
         num_classes : The number of classes that should be predicted
 
-        predict_conf_threshold : All predictions are filtered by this amount. E.g. if the model 
-                                predicts an object is present by 0.8 confidence, then we keep that 
+        predict_conf_threshold : All predictions are filtered by this amount. E.g. if the model
+                                predicts an object is present by 0.8 confidence, then we keep that
                                 as a positive prediction which could be passed to NMS. Otherwise,
                                 if confidence is < 0.75, it is is not passed on.
     '''
@@ -44,7 +44,11 @@ class ObjectDetection(torch.nn.Module):
         # The anchors sizes need to scale to cover the sizes of possible objects
         # in the dataset.
         # You can either set an absolute pixel value, or set based off size of image.
-        self.ANCHOR_SIZES = ((16, 32, 64),)
+        width = input_image_shape[-1]
+        self.ANCHOR_SIZES = ((width//8,
+                              width//4,
+                              width//2,
+                              width),)
 
         # These ratios are for all anchors
         self.ANCHOR_RATIOS = (1.0, 0.5, 2.0)
@@ -78,12 +82,19 @@ class ObjectDetection(torch.nn.Module):
             (logits.shape[0], logits.shape[1]), device=logits.device)
         class_targets = torch.zeros_like(logits)
 
+        positive_anchor_indices = []
+        positive_anchor_class_assignments = []
+        positive_anchor_confidences = []
         for idx, box_set in enumerate(batch.boxes):
             iou = boxops.anchor_box_iou(anchors, box_set)
             fgbg_mask[idx], pos_ind, assignments = boxops.create_label_matrix(iou,
                                                                               pos_threshold=self.pos_threshold,
                                                                               neg_threshold=self.neg_threshold)
+            positive_anchor_indices.append(pos_ind)
+            positive_anchor_class_assignments.append(assignments)
             class_targets[idx, pos_ind] = batch.labels[idx, assignments]
+            positive_anchor_confidences.append(
+                probs[idx, pos_ind, assignments])
 
         '''
         Calculate focal loss
@@ -110,9 +121,9 @@ class ObjectDetection(torch.nn.Module):
         in-model debugging
         '''
         if batch.debug == True:
-            print("Class Loss: Red {:.2f} Mean {:.2f} Shape: {}".format(
+            print("Class Loss: Red {:.4f} Mean {:.4f} Shape: {}".format(
                 class_loss_reduced, class_loss.mean(), class_loss.shape))
-            print("Class Loss Pos {:.2f} Neg {:.2f}".format(
+            print("Class Loss Pos {:.4f} Neg {:.4f}".format(
                 class_loss[pos_labeled_mask].mean(
                 ), class_loss[neg_labeled_mask].mean()
             ))
@@ -120,31 +131,40 @@ class ObjectDetection(torch.nn.Module):
             print("Mask shape: {}".format(fgbg_mask.shape))
             print("Class Probabilities {}".format(probs.shape))
             print("Class Target Shape: {}".format(class_targets.shape))
-            print("Pos Target Mean: {:.2f}".format(
-                class_targets[pos_labeled_mask, ...].sum(dim=-1).mean()))
 
+            # Uncomment to check that all positive anchors are receiving a target of "1.0"
+            # print("Pos Target Check: {:.2f}".format(
+            # class_targets[pos_labeled_mask, ...].sum(dim=-1).mean()))
+
+            # Prints the number of positive, neural, and negatively labeled anchors.
             print("Pos Anchors {} Neg Anchors {} Neutral Anchors {}".format(
                 torch.sum(fgbg_mask == 1), torch.sum(fgbg_mask == -1),
                 torch.sum(fgbg_mask == 0)
             ))
+
+            # Prints focal loss means
             print("Focal Weights Shape {} Pos Mean {:.3f} Neg Mean {:.3f}".format(focal_weights.shape,
                                                                                   focal_weights[pos_labeled_mask].mean(
                                                                                   ),
                                                                                   focal_weights[neg_labeled_mask].mean()))
-            val, _ = probs[pos_labeled_mask, ...].max(dim=-1)
-            print("Avg PosAnchor Confidence {}   {}".format(
-                val.mean(), val.shape))
+
+            # Uncomment to check that all ground truth boxes receive some anchor
+            # assignment (no NaN values in the printed tensor), and to monitor
+            # avg confidence. (This is plotted in Tensorboard so should probably be kept commented here)
+            # print("Avg PosAnchor Confidence {}".format(
+            #     torch.tensor([c.mean() for c in positive_anchor_confidences])))
             print("\n")
 
         losses = {
             'class_loss': class_loss_reduced
         }
+
         data = {
             'pos_predicted_anchors': [anchors[mask] for mask in pos_predicted_mask],
             'pos_predicted_confidence': [pos_predicted_conf[idx, mask] for idx, mask in enumerate(pos_predicted_mask)],
             'pos_predicted_labels': pos_predicted_targets,
             'pos_labeled_anchors': [anchors[mask] for mask in pos_labeled_mask],
-            'pos_labeled_confidence': probs[pos_labeled_mask],
+            'pos_labeled_confidence': positive_anchor_confidences,
         }
 
         return losses, data
