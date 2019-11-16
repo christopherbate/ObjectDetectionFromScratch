cb8597c5da088ee430bdf53b76b4fc797cb40c17
chris <chris@pop-os.localdomain>
2019-11-15 17:32:17 -0700

    Reorganize class_head prediction

--- models/class_head.py
+++ models/class_head.py
@@ -2,6 +2,41 @@ import torch
 import torch.nn as nn
 
 
+class BoxPrediction(torch.nn.Module):
+    '''
+        num_features: the number of features in each feature tensor
+        that this clas will receive as input
+    '''
+
+    def __init__(self,
+                 num_class,
+                 num_anchors,
+                 num_features,
+                 last_bias=-4.1,
+                 batch_norm=True, **kwargs):
+        super(BoxPrediction, self).__init__(**kwargs)
+        heads = [ClassHead(in_features=num,
+                           num_class=num_class,
+                           last_bias=last_bias,
+                           num_anchors=num_anchors[i],
+                           batch_norm=batch_norm)
+                 for i, num in enumerate(num_features)]
+        self.heads = nn.ModuleList(heads)
+        self.num_classes = num_class
+
+        print("Created Box Heads: ", heads)
+        print(num_features)
+
+    def forward(self, feature_maps):
+        logits = []
+        for idx, feature_map in enumerate(feature_maps):
+            layer_logits = self.heads[idx](feature_map)
+            logits.append(layer_logits)
+
+        logits = torch.cat(logits, dim=1)
+        return logits
+
+
 class ClassHead(torch.nn.Module):
     '''
     A series of 3 convolutional layers to regress
@@ -13,6 +48,7 @@ class ClassHead(torch.nn.Module):
                  num_class,
                  last_bias,
                  num_anchors,
+                 feature_depth=128,
                  kernel_size=(3, 3),
                  batch_norm=True,
                  **kwargs):
@@ -20,14 +56,14 @@ class ClassHead(torch.nn.Module):
         self.kernel_size = kernel_size
         self.padding = (kernel_size[0]//2, kernel_size[1]//2)
 
-        conv = [nn.Conv2d(in_features, 256,
+        conv = [nn.Conv2d(in_features, feature_depth,
                           kernel_size=self.kernel_size,
                           stride=1, padding=self.padding),
-                nn.Conv2d(256, 256,
+                nn.Conv2d(feature_depth, feature_depth,
                           kernel_size=self.kernel_size,
                           stride=1,
                           padding=self.padding),
-                nn.Conv2d(in_channels=256,
+                nn.Conv2d(in_channels=feature_depth,
                           out_channels=int(num_anchors)*int(num_class),
                           kernel_size=self.kernel_size, stride=1,
                           padding=self.padding, bias=True)]
@@ -63,38 +99,3 @@ class ClassHead(torch.nn.Module):
         out = out.reshape((out.shape[0], -1, self.num_classes))
 
         return out
-
-
-class BoxPrediction(torch.nn.Module):
-    '''
-        num_features: the number of features in each feature tensor
-        that this clas will receive as input
-    '''
-
-    def __init__(self,
-                 num_class,
-                 num_anchors,
-                 num_features,
-                 last_bias=-2.1,
-                 batch_norm=True, **kwargs):
-        super(BoxPrediction, self).__init__(**kwargs)
-        heads = [ClassHead(in_features=num,
-                           num_class=num_class,
-                           last_bias=last_bias,
-                           num_anchors=num_anchors[i],
-                           batch_norm=batch_norm)
-                 for i, num in enumerate(num_features)]
-        self.heads = nn.ModuleList(heads)
-        self.num_classes = num_class
-
-        print("Created Box Heads: ", heads)
-        print(num_features)
-
-    def forward(self, feature_maps):
-        logits = []
-        for idx, feature_map in enumerate(feature_maps):
-            layer_logits = self.heads[idx](feature_map)
-            logits.append(layer_logits)
-
-        logits = torch.cat(logits, dim=1)
-        return logits
